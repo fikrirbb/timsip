5468ae2a6b5a8ac471cb0c62aac626304dbad9f8
DESKTOP-8FE64MQ\Rini Kurriana Putri <kurriana@gmail.com>
2017-11-27 10:25:52 +0700

    perubahan database lagi

--- si_rotisip.sql
+++ si_rotisip.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2017 at 04:01 AM
+-- Generation Time: Nov 27, 2017 at 04:23 AM
 -- Server version: 5.6.26
 -- PHP Version: 5.5.28
 
@@ -43,6 +43,80 @@ INSERT INTO `barang` (`brg_id`, `brg_jenis`, `brg_nama`, `brg_harga`, `brg_foto`
 (105, 'kering', 'roti pisang', 3000, 'asd.jpeg', '2017-11-30'),
 (109, 'kering', 'roti coklat', 2500, 'eeea.jpeg', '2017-11-30');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang_masuk`
+--
+
+CREATE TABLE IF NOT EXISTS `barang_masuk` (
+  `ms_id` int(11) NOT NULL,
+  `ms_tgl` date NOT NULL,
+  `ms_jam` time NOT NULL,
+  `ms_totitem` int(11) NOT NULL,
+  `usr_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_barangmasuk`
+--
+
+CREATE TABLE IF NOT EXISTS `detail_barangmasuk` (
+  `ms_id` int(11) NOT NULL,
+  `brg_id` int(11) NOT NULL,
+  `dms_jumlah` int(11) NOT NULL,
+  `dms_stok` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_penjualan`
+--
+
+CREATE TABLE IF NOT EXISTS `detail_penjualan` (
+  `pj_id` int(11) NOT NULL,
+  `brg_id` int(11) NOT NULL,
+  `dpj_jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE IF NOT EXISTS `penjualan` (
+  `pj_id` int(11) NOT NULL,
+  `pj_tgl` date NOT NULL,
+  `pj_jam` time NOT NULL,
+  `pj_totitem` int(11) NOT NULL,
+  `pj_cash` int(11) NOT NULL,
+  `pj_kembali` int(11) NOT NULL,
+  `usr_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `usr_id` int(11) NOT NULL,
+  `usr_pass` varchar(10) NOT NULL,
+  `usr_nama` varchar(10) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`usr_id`, `usr_pass`, `usr_nama`) VALUES
+(1, '12345', 'Rini');
+
 --
 -- Indexes for dumped tables
 --
@@ -54,6 +128,36 @@ ALTER TABLE `barang`
   ADD PRIMARY KEY (`brg_id`);
 
 --
+-- Indexes for table `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  ADD PRIMARY KEY (`ms_id`,`usr_id`);
+
+--
+-- Indexes for table `detail_barangmasuk`
+--
+ALTER TABLE `detail_barangmasuk`
+  ADD PRIMARY KEY (`ms_id`,`brg_id`);
+
+--
+-- Indexes for table `detail_penjualan`
+--
+ALTER TABLE `detail_penjualan`
+  ADD PRIMARY KEY (`pj_id`,`brg_id`);
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`pj_id`,`usr_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`usr_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -62,6 +166,31 @@ ALTER TABLE `barang`
 --
 ALTER TABLE `barang`
   MODIFY `brg_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=110;
+--
+-- AUTO_INCREMENT for table `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  MODIFY `ms_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `detail_barangmasuk`
+--
+ALTER TABLE `detail_barangmasuk`
+  MODIFY `ms_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `detail_penjualan`
+--
+ALTER TABLE `detail_penjualan`
+  MODIFY `pj_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `pj_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `usr_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
